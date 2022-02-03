b675a85e362f5bdd91870e1dfd839e26bc03f3e2
akitoshi.shiga <akitoshi.shiga@insight-inc.biz>
2022-02-03 23:35:06 +0900

    model

--- 12/sample_a/src/App.java
+++ 12/sample_a/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BeatModelInterface model       = new BeatModel();
+        ControllerInterface controller = new BeatController(model);
     }
 }
